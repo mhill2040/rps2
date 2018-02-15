29721cb5d10a5cd7242edd5559dc20583f0d2dea
Melissa Hill <mhill2040@gmail.com>
2018-02-14 17:02:12 -0700

    wip

---
+++ main.js
@@ -0,0 +1,13 @@
+/* Rebuild of Rock Paper Scissors
+
+You will be beginning a new project NOT refactoring your old project.
+
+Basic Objectives:
+Create your html, css, and javascript file inside a folder
+Create a git repository for your project and link it to a new GitHub repository.
+Add jQuery to your project
+User selects rock, paper, or scissors
+Computer randomly chooses
+hint: have your choices in an array and randomly choose
+DISPLAY the user and computers choice on the screen
+DISPALY the winner */
